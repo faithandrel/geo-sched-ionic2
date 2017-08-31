ff1abaf502aaf55f8fe1845c08a5882dc23b2345
faithandrel <f.macad@gmail.com>
2017-09-01 01:12:36 +0800

    Comments on item

--- src/pages/item/item.ts
+++ src/pages/item/item.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { NavController, NavParams, Nav } from 'ionic-angular';
+import { NavController, NavParams, Nav, LoadingController } from 'ionic-angular';
 import { FormBuilder, Validators } from '@angular/forms';
 
 import { BackEndService } from '../../services/back-end-service';
@@ -21,13 +21,14 @@ export class ItemPage {
   constructor(private nav: NavController, 
                       navParams: NavParams,
               private myNav: Nav,
+              public loadingCtrl: LoadingController,
               private backEndService: BackEndService,
               private formBuilder: FormBuilder,
               private schdLocation: SchdLocation,
               private schdErrorHandler: SchdErrorHandler) {
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
-
+    this.myResults = JSON.stringify(this.selectedItem);
     this.commentForm = this.formBuilder.group({
       content: ['', Validators.required],
     });
@@ -52,6 +53,9 @@ export class ItemPage {
   }
   
   itemsTest() {
+    let loader = this.loadingCtrl.create({content: 'Loading...'});
+    loader.present()
+
     let newItem = {
       title: '',
       content: this.commentForm.get('content').value,
@@ -67,7 +71,11 @@ export class ItemPage {
       return this.backEndService.saveItem(item);
     })
     .then(res => {
-        this.schdErrorHandler.showSchdError(res);
+      return this.backEndService.getSingleItem(this.selectedItem.id);
+    })
+    .then(res => {
+      this.selectedItem = res;
+      loader.dismiss();
     })
     .catch(error => {
         this.schdErrorHandler.showSchdError(error);
